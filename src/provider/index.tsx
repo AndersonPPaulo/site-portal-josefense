d545f85faac7541024741657e31f662070df24a3
Victor Fausto <94501287+JoaumVictor@users.noreply.github.com>
2026-01-07 14:14:21 -0300

    feat: adicionar seção de colunistas e otimizar a estrutura do provedor

--- src/provider/index.tsx
+++ src/provider/index.tsx
@@ -5,23 +5,26 @@ import { BannerAnalyticsProvider } from "./analytics/banner";
 import { CompanyAnalyticsProvider } from "./analytics/company";
 import { ArticleProvider } from "./article";
 import { BannerProvider } from "./banner";
+import { ColumnistProvider } from "./columnist";
 import { PublicCompanyProvider } from "./company";
 import { PortalProvider } from "./portal";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ArticleProvider>
-      <PublicCompanyProvider>
-        <CompanyAnalyticsProvider>
-          <BannerProvider>
-            <ArticleAnalyticsProvider>
-              <BannerAnalyticsProvider>
-                <PortalProvider>{children}</PortalProvider>
-              </BannerAnalyticsProvider>
-            </ArticleAnalyticsProvider>
-          </BannerProvider>
-        </CompanyAnalyticsProvider>
-      </PublicCompanyProvider>
+      <ColumnistProvider>
+        <PublicCompanyProvider>
+          <CompanyAnalyticsProvider>
+            <BannerProvider>
+              <ArticleAnalyticsProvider>
+                <BannerAnalyticsProvider>
+                  <PortalProvider>{children}</PortalProvider>
+                </BannerAnalyticsProvider>
+              </ArticleAnalyticsProvider>
+            </BannerProvider>
+          </CompanyAnalyticsProvider>
+        </PublicCompanyProvider>
+      </ColumnistProvider>
     </ArticleProvider>
   );
 }
